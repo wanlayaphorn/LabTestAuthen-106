bbf3f2b13824d8c34b6ae742c72b85d6b2e2feef
Wanlayaphorn <632021106@tsu.ac.th>
2023-03-01 10:03:32 +0700

    FirebaseTest-106

---
+++ authentest_106/android/app/src/main/kotlin/com/example/authentest_106/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.authentest_106
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ authentest_106/lib/login.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class login extends StatefulWidget {
+  const login({super.key});
+
+  @override
+  _MyLoginPageState createState() => _MyLoginPageState();
+}
+
+class _MyLoginPageState extends State<login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(" login"),
+       ) body: Container(
+            color: Colors.green[50],
+            child: Center(
+              child: Container(
+                
+          
+                  )),
+            ));
+  }
+
+  
+  }
